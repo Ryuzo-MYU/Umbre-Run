0f5243f69211848744caa8b40675b9fd3f3de410
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-02-12 23:08:09 +0900

    実プレイ用ステージを構成。バグの修正

--- Assets/Scripts/Gimmick/Rain.cs
+++ Assets/Scripts/Gimmick/Rain.cs
@@ -6,7 +6,7 @@ public class Rain : Gimmick
     [SerializeField] Vector3 offset;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip audioClip;
-    protected override void GimmickFailed()
+    protected override void GimmickFailed(GameObject gameobject)
     {
         Debug.Log("Rain起動");
         BackCollisionedTarget();
@@ -19,25 +19,25 @@ public class Rain : Gimmick
     private void BackCollisionedTarget()
     {
         // Playerの現在の座標
-        Vector3 playerCurrentHorizontalPos = player.transform.position;
+        Vector3 playerCurrentPos = player.transform.position;
         // Chaserの現在の座標
-        Vector3 chaserCurrentHorizontalPos = chaser.transform.position;
+        Vector3 chaserCurrentPos = chaser.transform.position;
         // Rainによって戻される予定のPlayerの座標
-        Vector3 playerNextHorizontalPos = playerCurrentHorizontalPos + offset;
+        Vector3 playerNextPos = playerCurrentPos + offset;
 
         // 変更後のPlayerのx座標 ≦ Chaserの現在位置 の場合
-        if (playerNextHorizontalPos.x <= chaserCurrentHorizontalPos.x)
+        if (playerNextPos.x <= chaserCurrentPos.x)
         {
+            Debug.Log("PlayerをChaserと同じ位置に");
             // Playerの位置をChaserと同じ位置に
-            player.transform.position = chaserCurrentHorizontalPos;
+            player.transform.position = chaserCurrentPos;
         }
-        // Chaserの現在位置 ≦ 変更後のPlayerのx座標 の場合
+        // Chaserの現在位置 < 変更後のPlayerのx座標 の場合
         else
         {
+            Debug.Log("Playerを少し前の位置に");
             // Player の位置をoffset分戻す
             player.transform.position += offset;
         }
-
-
     }
 }
--- Assets/Scripts/Stage/DeathBox.cs
+++ Assets/Scripts/Stage/DeathBox.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class DeathBox : MonoBehaviour
 {
+    [SerializeField] Vector3 offset;
+
+    // 衝突時、衝突対象を指定の位置まで戻す 
     private void OnCollisionEnter2D(Collision2D other)
     {
-
+        GameObject target = other.gameObject;
+        backPreviousPos(target);
     }
+
+    // 対象をoffsetの分だけ移動させる
     private void backPreviousPos(GameObject target)
     {
-        
+        Debug.Log("Playerを少し前の位置に");
+        Transform targetTransform = target.transform;
+        targetTransform.position += offset;
     }
 }
